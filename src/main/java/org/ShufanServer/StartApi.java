41fb313572f7af3d2e139973d2db93269d21b88c
ShufanSun <ssun329@wisc.edu>
2023-09-27 21:06:05 -0500

    Initial commit

---
+++ src/main/java/org/ShufanServer/StartApi.java
@@ -0,0 +1,12 @@
+package org.ShufanServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@author Shufan Sun
+//This is the main class of the rest API I created using springboot and Maven
+@SpringBootApplication
+public class StartApi {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApi.class, args);
+        }
+    }
