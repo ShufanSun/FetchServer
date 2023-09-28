e12886802c2e0b38db71f3d14a74d0c9823607f3
ShufanSun <ssun329@wisc.edu>
2023-09-28 15:39:39 -0500

    Added comments

--- src/main/java/org/ShufanServer/repositories/PointsRepository.java
+++ src/main/java/org/ShufanServer/repositories/PointsRepository.java
@@ -4,6 +4,10 @@ import org.ShufanServer.model.Points;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing Points entities. It extends JpaRepository
+ * to provide basic CRUD operations for the Points entity.
+ */
 @Repository
 public interface PointsRepository extends JpaRepository<Points, Integer> {
 }
